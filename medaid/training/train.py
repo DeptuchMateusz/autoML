90326a08bba8f4fb47198997890670c1476d9897
DeptuchMateusz <135151523+DeptuchMateusz@users.noreply.github.com>
2025-01-13 21:17:55 +0100

    male poprawki

--- medaid/training/train.py
+++ medaid/training/train.py
@@ -1,4 +1,4 @@
-from sklearn.metrics import f1_score, precision_score, recall_score
+from sklearn.metrics import f1_score, precision_score, recall_score, make_scorer, accuracy_score
 
 from medaid.training.search import CustomRandomizedSearchCV, CustomGridSearchCV
 import os
@@ -9,6 +9,9 @@ from xgboost import XGBClassifier
 from lightgbm import LGBMClassifier
 import pandas as pd
 from sklearn.metrics import get_scorer
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning, message=".*ConvergenceWarning.*")
+
 
 
 # TODO: siatki parametrów
@@ -16,13 +19,16 @@ from sklearn.metrics import get_scorer
 
 
 def train(X, y, X_test, y_test, models, metric, path, search, cv, n_iter):
+    warnings.filterwarnings("ignore", category=UserWarning, message=".*ConvergenceWarning.*")
 
     param_grids = {
         "logistic": {
             'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000],
-            'penalty': ['l1', 'l2'],
-            'solver': ['liblinear',
-                      # 'saga' # ten dziad jest wkurzajacy: wykorzystuje ciagle max_iter
+            'penalty': ['l2'],
+            'solver': [
+                       'saga',
+                       'lbfgs',
+                        'newton-cg'
                        ],
             #'max_iter': [100]
         },
@@ -32,11 +38,11 @@ def train(X, y, X_test, y_test, models, metric, path, search, cv, n_iter):
             'min_samples_leaf': [1, 2, 3, 4, 5]
         },
         "random_forest": {
-            'n_estimators': [100, 300, 400],
-            'max_depth': [3, 5, 8, 11, 15],
+            'n_estimators': [100, 200, 300],
+            'max_depth': [3, 5, 8, 11],
             'min_samples_split': [2, 4, 6, 8],
             'min_samples_leaf': [1, 3, 5],
-           # 'n_jobs': [-1]
+            # 'n_jobs': [-1]
         },
         "xgboost": {
             'verbosity': [0],
@@ -77,22 +83,29 @@ def train(X, y, X_test, y_test, models, metric, path, search, cv, n_iter):
     for model in models:
         param_grid = param_grids[model]
         if model == "logistic":
-            model_with_params = LogisticRegression()
+            model_with_params = LogisticRegression(n_jobs=-1, max_iter=1000)
         elif model == "tree":
             model_with_params = DecisionTreeClassifier()
         elif model == "random_forest":
-            model_with_params = RandomForestClassifier()
+            model_with_params = RandomForestClassifier(n_jobs=-1)
         elif model == "xgboost":
-            model_with_params = XGBClassifier()
+            model_with_params = XGBClassifier(n_jobs=-1)
         elif model == "lightgbm":
-            model_with_params = LGBMClassifier()
+            model_with_params = LGBMClassifier(n_jobs=-1)
         if search == "random":
             rs = CustomRandomizedSearchCV(model_with_params, param_grid, n_iter=n_iter, cv=cv,
-                                          scoring={'f1': 'f1', 'accuracy': 'accuracy', 'precision': 'precision', 'recall': 'recall'},
-                                          refit = metric, name=model)
+                                          scoring={'f1': make_scorer(f1_score, average='weighted'),
+                                                   'accuracy': make_scorer(accuracy_score),
+                                                   'precision': make_scorer(precision_score, average='weighted'),
+                                                   'recall': make_scorer(recall_score, average='weighted')},
+                                          refit = metric, name=model,
+                                          n_jobs=-1)
         else:
             rs = CustomGridSearchCV(model_with_params, param_grid,  cv=cv,
-                                          scoring={'f1': 'f1', 'accuracy': 'accuracy', 'precision': 'precision', 'recall': 'recall'},
+                                          scoring={'f1': make_scorer(f1_score, average='weighted'),
+                                                   'accuracy': make_scorer(accuracy_score),
+                                                   'precision': make_scorer(precision_score, average='weighted'),
+                                                   'recall': make_scorer(recall_score, average='weighted')},
                                           refit = metric, name=model)
 
         rs.fit(X, y)
@@ -112,15 +125,18 @@ def train(X, y, X_test, y_test, models, metric, path, search, cv, n_iter):
             'precision': rs.cv_results_['mean_test_precision'][rs.best_index_],
             'recall': rs.cv_results_['mean_test_recall'][rs.best_index_],
             'test_best_score': test_best_score,
-            'test_f1': f1_score(y_test, rs.best_estimator_.predict(X_test)),
-            'test_accuracy': rs.best_estimator_.score(X_test, y_test),
-            'test_precision': precision_score(y_test, rs.best_estimator_.predict(X_test)),
-            'test_recall': recall_score(y_test, rs.best_estimator_.predict(X_test))
+            'test_f1': f1_score(y_test, rs.best_estimator_.predict(X_test), average='weighted'),
+            'test_accuracy': accuracy_score(y_test, rs.best_estimator_.predict(X_test)),
+            'test_precision': precision_score(y_test, rs.best_estimator_.predict(X_test), average='weighted'),
+            'test_recall': recall_score(y_test, rs.best_estimator_.predict(X_test), average='weighted')
         }
         metrics_list.append(best_metrics)
 
-        best_models = [x for _, x in sorted(zip(best_models_scores, best_models), reverse=True)]
-        best_models_scores = sorted(best_models_scores, reverse=True)
+        best_models_scores, best_models = zip(
+            *sorted(zip(best_models_scores, best_models), key=lambda pair: pair[0], reverse=True)
+        )
+        best_models = list(best_models)
+        best_models_scores = list(best_models_scores)
         metrics_df = pd.DataFrame(metrics_list).sort_values(by='best_score', ascending=False)
 
     return best_models, best_models_scores, metrics_df
