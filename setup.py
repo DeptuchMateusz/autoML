a3f3ac64733a044bc88f9c8026058150bce58a1c
DeptuchMateusz <135151523+DeptuchMateusz@users.noreply.github.com>
2025-01-18 17:22:37 +0100

    req

--- setup.py
+++ setup.py
@@ -4,21 +4,21 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-# Get the long description from the README file
 with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
+
 setup(
     name="medAId",
-    version="0.0.1",
-    description="Automated Machine Learning for Doctors",
+    version="0.1.0",
+    description="Automated Machine Learning for medical use",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/DeptuchMateusz/autoML",
+    url="https://github.com/DeptuchMateusz/medAId",
     author="Zofia Kamińska, Karolina Dunal, Mateusz Deptuch",
     license="MIT",
     packages=find_packages(exclude=["tests", "*.tests", "*.tests.*"]),
-    install_requires=open("requirements.txt").readlines(),
+    install_requires=open('requirements.txt').read().splitlines(),
     include_package_data=True,
     python_requires='>=3.8',
     keywords=[
@@ -28,5 +28,5 @@ setup(
         "medical data",
     ],
     test_suite="tests",
-    tests_require=["pytest", "unittest2"],  # Użyj odpowiednich narzędzi do testowania
+    tests_require=["pytest", "unittest2"],
 )
