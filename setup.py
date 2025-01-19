79c2c8d8e2a5d5857d752ec3250c4310e15c267f
DeptuchMateusz <135151523+DeptuchMateusz@users.noreply.github.com>
2025-01-19 19:22:08 +0100

    update gitignore and version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("requirements.txt", 'r', encoding='utf-8') as f:
 
 setup(
     name="medaid",
-    version="0.1.6",
+    version="0.1.7",
     description="Automated Machine Learning for medical use",
     long_description=long_description,
     long_description_content_type="text/markdown",
